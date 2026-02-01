8b9235623665f52b9b77c3d2526a9f05835861d7
jamedinac <jhersonmedina2505@gmail.com>
2026-02-01 09:34:54 -0500

    Update data writer

--- mbc/src/main/java/GeneExpressionDataOperation/GeneExpressionDataWrite.java
+++ mbc/src/main/java/GeneExpressionDataOperation/GeneExpressionDataWrite.java
@@ -6,19 +6,29 @@ import Interfaces.IGeneExpressionDataWrite;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class GeneExpressionDataWrite implements IGeneExpressionDataWrite {
 
     @Override
     public void writeClusteringDataToFile(GeneClusteringResult geneClusteringResult, String fileName) {
         try {
-            File file = new File(fileName);
+            String geneClusteringResultFilePath = fileName + "\\output.txt";
+            StringBuilder fileContent = new StringBuilder();
 
-            file.createNewFile();
+            for (int g = 0; g < geneClusteringResult.getNumberOfGenes(); g++) {
+                fileContent.append(geneClusteringResult.getGeneExpressionData().getGeneId(g)).append("\t");
 
-            FileWriter writer = new FileWriter(file);
-            writer.write("Hello World!");
+                for (int c = 0; c < geneClusteringResult.getNumberOfClusters(); c++) {
+                    fileContent.append(geneClusteringResult.getGeneClusteringData()[g][c]).append("\t");
+                }
 
+                fileContent.append("\n");
+            }
+
+            Files.writeString(Paths.get(geneClusteringResultFilePath), fileContent.toString());
         } catch (IOException e) {
             System.out.println("Error writing gene expression file: " + e.getMessage());
         }

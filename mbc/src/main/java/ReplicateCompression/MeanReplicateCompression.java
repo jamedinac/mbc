95a861701d8ccee8687623fbd3b3cdc6b02f29be
jamedinac <jhersonmedina2505@gmail.com>
2026-06-27 09:51:43 -0500

    Remove blank line

--- mbc/src/main/java/ReplicateCompression/MeanReplicateCompression.java
+++ mbc/src/main/java/ReplicateCompression/MeanReplicateCompression.java
@@ -1,14 +1,13 @@
 package ReplicateCompression;
 
-
 import java.util.ArrayList;
 
 public class MeanReplicateCompression extends AReplicateCompression {
-    protected double calculate(ArrayList<Double> replicates){
+    protected double calculate(ArrayList<Double> replicates) {
         double mean = 0.0;
-        for (Double replicate : replicates){
+        for (Double replicate : replicates) {
             mean += replicate;
         }
-        return mean/replicates.size();
+        return mean / replicates.size();
     }
 }
